a8ab86c289bb3f2cd736bed9639dfae2ca072d0e
PereiraGus <gustavo.oito@hotmail.com>
2024-05-31 11:06:47 -0300

    Módulo landing + configuração FontAwesome.

--- sustentare/src/index.js
+++ sustentare/src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "./globals.css"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fas } from "@fortawesome/free-solid-svg-icons"
+import { fab } from "@fortawesome/free-brands-svg-icons"
 
+import "./globals.css"
 import Main from './Main';
+
+library.add(fas, fab)
 
 export default function App() {
   return (
--- sustentare/src/sections/landing/Landing.jsx
+++ sustentare/src/sections/landing/Landing.jsx
@@ -1,5 +1,6 @@
 import styles from "./Landing.module.css"
 import telefone from "../../assets/cellphone.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function Landing() {
     return (
@@ -7,15 +8,21 @@ export default function Landing() {
             <section className={styles.landing}>
                 <div>
                     <div>
-                        <h1>Tecnologia de ponta, na palma das suas mãos.</h1>
+                        <h1>Tecnologia de ponta,<br/>na palma das suas mãos.</h1>
                         <p>
                             De projetos simples a complexos, a Sustentare te ajuda
                             a solucionar problemas e a tornar seus projetos realidade.
                         </p>
-                        <button>Nosso email</button>
+                        <button>
+                            <FontAwesomeIcon icon="envelope-open-text" className="icon"/>    
+                            Nosso email
+                        </button>
                     </div>
                     <div>
-                        <img src={telefone}/>
+                        <img 
+                        src={telefone} 
+                        alt="Imagem de um celular com nosso aplicativo Paralelo 19."
+                        />
                     </div>
                 </div>
             </section>
