18883297417411c56debf80a62768988b6480512
PereiraGus <gustavo.oito@hotmail.com>
2024-05-31 11:31:19 -0300

    Seção de skills

--- sustentare/src/Main.jsx
+++ sustentare/src/Main.jsx
@@ -1,11 +1,13 @@
 import Header from "./sections/header/Header";
 import Landing from "./sections/landing/Landing";
+import Skills from "./sections/skills/Skills"
 
 export default function Main(){
     return (
         <>
             <Header />
             <Landing />
+            <Skills />
         </>
     )
 }
