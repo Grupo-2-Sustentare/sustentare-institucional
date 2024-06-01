12e7b744e46947c43407ba6cdece642a0321debf
PereiraGus <gustavo.oito@hotmail.com>
2024-06-01 11:02:27 -0300

    Finalização da seção valores

--- sustentare/src/components/ValueCard.jsx
+++ sustentare/src/components/ValueCard.jsx
@@ -1,9 +1,21 @@
 import styles from "./ValueCard.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-export default function ValueCard(){
+export default function ValueCard({ titulo, corpo, topicos }) {
     return (
         <>
-        
+            <div className={styles.valueCard}>
+                <h4>{titulo}</h4>
+                <p>{corpo}</p>
+                <ul>
+                    {topicos.map((x) => 
+                        <li>
+                            <FontAwesomeIcon icon="fa-solid fa-circle-check" className={styles.icon} />
+                            <p>{x}</p>
+                        </li>
+                    )}
+                </ul>
+            </div>
         </>
     )
 }
--- sustentare/src/sections/values/Values.jsx
+++ sustentare/src/sections/values/Values.jsx
@@ -2,23 +2,26 @@ import styles from "./Values.module.css"
 import values from "../../assets/values.json"
 import ValueCard from "../../components/ValueCard"
 
-export default function Values(){
+export default function Values() {
     return (
         <>
-            <h3>Nossos valores</h3>
-            <p>
-                Temos sede por resolução de problemas. Existimos para tornar os avanços tecnológicos mais acessíveis, práticos.
-            </p>
-            <div>
-                {values.map(
-                    (x) =>
-                        <ValueCard
-                            titulo={x.titulo}
-                            corpo={x.corpo}
-                            topicos={x.topicos}
-                        />
-                )}
-            </div>
+            <section className={styles.values}>
+                <h3>Nossos valores</h3>
+                <hr />
+                <p>
+                    Temos sede por resolução de problemas. Existimos para tornar os avanços tecnológicos mais acessíveis, práticos.
+                </p>
+                <div>
+                    {values.map(
+                        (x) =>
+                            <ValueCard
+                                titulo={x.titulo}
+                                corpo={x.corpo}
+                                topicos={x.topicos}
+                            />
+                    )}
+                </div>
+            </section>
         </>
     )
 }
